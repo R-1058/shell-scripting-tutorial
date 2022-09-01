0ed7cdce3cdfc493a61ffe5a79c0b7279752f1dc
R-1058 <100771689+R-1058@users.noreply.github.com>
2022-09-01 13:36:30 +0530

    Update CpuMemDisk.sh

--- CpuMemDisk.sh
+++ CpuMemDisk.sh
@@ -13,16 +13,5 @@ CPUUSAGE=$(top -b -n 2 -d1 | grep "Cpu(s)" | tail -n1 | awk '{print $2}' |awk -F
 MEMUSAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
 DISKUSAGE=$(df -h / | awk '{print $5}' |tail -n 1 |sed 's/%//g')
 
-echo 'HostName, Date&Time, CPU(%), MEM(%), DISK(%)' >> /opt/usagereport
-echo "$HOSTNAME, $DATET, $CPUUSAGE, $MEMUSAGE, $DISKUSAGE" >> /opt/usagereport
-
-for i in `cat /scripts/hostlist`
-do
-RHOST=$(ssh $i hostname)
-RDATET=$(ssh $i 'date "+%Y-%m-%d %H:%M:%S"')
-RCPUUSAGE=$(ssh $i top -b -n 2 -d1 | grep "Cpu(s)" | tail -n1 | awk '{print $2}' |awk -F. '{print $1}')
-RMEMUSAGE=$(ssh $i free | grep Mem | awk '{print $3/$2 * 100.0}')
-RDISKUSAGE=$(ssh $i df -P / |column -t | awk '{print $5}' |tail -n 1 |sed 's/%//g')
-
-echo "$RHOST, $RDATET, $RCPUUSAGE, $RMEMUSAGE, $RDISKUSAGE" >> /opt/usagereport
-done
+echo 'HostName, Date&Time, CPU(%), MEM(%), DISK(%)' >> /home/ubuntu/report.txt
+echo "$HOSTNAME, $DATET, $CPUUSAGE, $MEMUSAGE, $DISKUSAGE" >> /home/ubuntu/report1.txt
